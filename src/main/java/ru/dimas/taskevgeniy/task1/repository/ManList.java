f89d7fbb218c89bda94f61e303044ef58e2320b8
Dimas <shelop_1@mail.ru>
2024-11-07 15:38:25 +0800

    izm new methods add Man in ManList

--- src/main/java/ru/dimas/taskevgeniy/task1/repository/ManList.java
+++ src/main/java/ru/dimas/taskevgeniy/task1/repository/ManList.java
@@ -5,10 +5,12 @@ import ru.dimas.taskevgeniy.task1.model.Man;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
 public class ManList {
     private int countMan;
     private final List<Man> manList = new ArrayList<>();
+
     {
         manList.add(new Man(++countMan, 25, "Stas"));
         manList.add(new Man(++countMan, 10, "Anton"));
@@ -16,26 +18,27 @@ public class ManList {
         manList.add(new Man(++countMan, 12, "Maxim"));
     }
 
-    public Man getMan(int id){
-        return this.manList.stream().filter(man->man.getId()==id).findAny().orElse(null);
+    public Man getMan(int id) {
+        return this.manList.stream().filter(man -> man.getId() == id).findAny().orElse(null);
     }
 
-    public void addMan(Man man){
+    public void addMan(Man man) {
         man.setId(++countMan);
         manList.add(man);
 
     }
 
-    public List<Man> getManList(){
+    public List<Man> getManList() {
         return manList;
     }
 
-    public void deleteMan(int id){
+    public void deleteMan(int id) {
         manList.remove(id);
     }
-    public void updateMan(int id, Man man){
-        id = id-1;
-        if (id>=manList.size() || id<0){
+
+    public void updateMan(int id, Man man) {
+        id = id - 1;
+        if (id >= manList.size() || id < 0) {
             return;
         }
         manList.get(id).setAge(man.getAge());
